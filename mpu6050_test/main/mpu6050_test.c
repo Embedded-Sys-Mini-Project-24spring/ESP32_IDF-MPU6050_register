085b768b4b8f2da5b397992c483f2f818f4c205e
kosumi <chankocyo@gmail.com>
2024-04-02 15:47:18 -0600

    Merge serial output to a single line

--- mpu6050_test/main/mpu6050_test.c
+++ mpu6050_test/main/mpu6050_test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "esp_err.h"
 #include "mpu6050_driver.h"
 #include "freertos/FreeRTOS.h"
 
@@ -8,18 +9,21 @@ void app_main(void)
     measurement_out_t measurement_out;
     int cnt = 0;
 
-    mpu6050_init();
+    esp_err_t err = mpu6050_init();
+    // while (err != ESP_OK) {
+    //     printf("Fuck, init fail \n");
+    // }
 
     while (1)
     {
         measurement_out = mpu6050_get_value();
         printf("accel_xout:%d\t;", measurement_out.accel_out.accel_xout);
         printf("accel_yout:%d\t;", measurement_out.accel_out.accel_yout);
-        printf("accel_zout:%d;\n", measurement_out.accel_out.accel_zout);
+        printf("accel_zout:%d\t;", measurement_out.accel_out.accel_zout);
         printf("gyro_xout:%d\t;", measurement_out.gyro_out.gyro_xout);
         printf("gyro_yout:%d\t;", measurement_out.gyro_out.gyro_yout);
-        printf("gyro_zout:%d;\n", measurement_out.gyro_out.gyro_zout);
-        printf("temp:%d;\n", measurement_out.temp_out.temp_xout);
+        printf("gyro_zout:%d\t;", measurement_out.gyro_out.gyro_zout);
+        printf("temp:%d\t;", measurement_out.temp_out.temp_xout);
         printf("cnt:%d\n", cnt++);
         vTaskDelay(100 / portTICK_PERIOD_MS); 
     }
