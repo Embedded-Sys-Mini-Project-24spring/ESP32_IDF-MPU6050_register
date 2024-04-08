be235edd992f728775939268d7064f9b54fb6672
Noah Mecham <nmecham27@gmail.com>
2024-04-07 20:41:15 -0600

    Adding floating point smoothing filter function.

--- mpu6050_test/main/smoothing_filter.c
+++ mpu6050_test/main/smoothing_filter.c
@@ -15,3 +15,16 @@ void FilterData( int16_t* data, uint8_t dataSize, int16_t* filteredData )
         *filteredData = (int16_t)(tempFilteredValue >> 16);
     }
 }
+
+void FilterDataFloating( double* data, uint8_t dataSize, double* filteredData )
+{
+    if(dataSize != 5)
+    {
+        printf("Invalid number of samples attempted to be filtered.");
+    }
+    else
+    {
+        //Filter the data
+        *filteredData = ( (-3*data[0]) + (12*data[1]) + (17*data[2]) + (12*data[3]) + (-3*data[4]) )/35;
+    }
+}
--- mpu6050_test/main/smoothing_filter.h
+++ mpu6050_test/main/smoothing_filter.h
@@ -7,3 +7,8 @@
 // FilterData will return the filtered results of the provided
 // data. The provided data must be 5 samples deep.
 void FilterData( int16_t* data, uint8_t dataSize, int16_t* filteredData );
+
+// FilterData will return the filtered results of the provided
+// data. The provided data must be 5 samples deep. This is a
+// version of the function that handles floating point values
+void FilterDataFloating( double* data, uint8_t dataSize, double* filteredData );
